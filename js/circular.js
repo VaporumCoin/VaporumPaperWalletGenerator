4b89e164fdedf0dfb976e993b1e2ce86422e25a0
webworker01 <webworker@live.com>
2018-06-20 19:12:06 -0400

    Add progress % number, fix missing tab, organize html better,

--- js/circular.js
+++ js/circular.js
@@ -5,31 +5,38 @@ var randomBar = new ProgressBar.Circle('#random-bar', {
     easing: 'easeOut',
     strokeWidth: 5,
     fill: '#2F6565',
+    text: {
+        value: '0%',
+        className: 'randombar_finished'
+    }
 });
 
 var randomseeder = function(evt) {
     if (!evt) var evt = window.event;
     var timeStamp = new Date().getTime();
 
-    if (ninja.seeder.seedCount >= ninja.seeder.seedLimit) {
+    if (ninja.seeder.seedCount == ninja.seeder.seedLimit) {
+
         document.removeEventListener('mousemove', randomseeder);
-        document.removeEventListener('keypress', randomseeder); 
+        document.removeEventListener('keypress', randomseeder);
+        // document.removeEventListener('mousemove', arguments.callee, false);
+        // document.removeEventListener('keypress', arguments.callee, false);
+
         ninja.seeder.seedCount++;
         randomBar.setText('100%');
-        // randomBar.text.style.marginLeft = 0;
-        // randomBar.text.style.marginTop = 0;
-        randomBar.text.className += ' randombar_finished';
+        // randomBar.text.className += ' randombar_finished';
 
-        document.querySelector('#generateseed').style.opacity = 0;
-        document.querySelector('#generateseed').style.visibility = 'hidden';
+        document.querySelector('#generatingseed').style.opacity = 0;
+        document.querySelector('#generatingseed').style.visibility = 'hidden';
 
-        setInterval(function() {
+        setTimeout(function() {
             ninja.wallets.singlewallet.open();
             document.querySelector('#menu').style.visibility = 'visible';
             document.querySelector('#generatetext').style.display = 'none';
             document.querySelector('#generatedtext').style.display = 'block';
             document.querySelector('#generatedheader').style.display = 'block';
-            document.querySelector('#generateseed').style.display = 'none';
+            document.querySelector('#generatingseed').style.display = 'none';
+            document.querySelector('#random-innertext').innerHTML = '';
         }, 1000)
     } else if ((ninja.seeder.seedCount < ninja.seeder.seedLimit) && evt && (timeStamp - ninja.seeder.lastInputTime) > 40) {
         SecureRandom.seedTime();
@@ -51,6 +58,7 @@ var randomseeder = function(evt) {
         var randomTop = (Math.round(Math.random()) * 2 - 1) * Math.floor(Math.random() * 77);
 
         randomBar.animate(progress);
+        randomBar.setText(Math.floor(progress * 100) + '%');
 
         var element = document.createElement("div");
         element.className = 'randombar_text';
@@ -61,13 +69,10 @@ var randomseeder = function(evt) {
         element.style.opacity = 1;
         element.style.visibility = 'visible';
 
-        setInterval(function() {
+        setTimeout(function() {
             element.style.opacity = 0;
             element.style.visibility = 'hidden';
         }, 100)
-
-        // randomBar.text.style.marginLeft = randomLeft + 'px';
-        // randomBar.text.style.marginTop = randomTop + 'px';
 
         // console.log(Crypto.util.bytesToHex(SecureRandom.pool)); 
     }
@@ -75,7 +80,7 @@ var randomseeder = function(evt) {
 
 document.querySelector('#newaddresshero').addEventListener('click', function() {
     document.querySelector('#generatedheader').style.display = 'none';
-    document.querySelector('#generateseed').style.display = 'block';
-    document.addEventListener('mousemove', randomseeder);
-    document.addEventListener('keypress', randomseeder); 
+    document.querySelector('#generatingseed').style.display = 'block';
+    document.addEventListener('mousemove', randomseeder, false);
+    document.addEventListener('keypress', randomseeder, false); 
 });
